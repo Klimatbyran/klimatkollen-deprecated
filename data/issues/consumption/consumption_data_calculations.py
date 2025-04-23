4833acf232c0b70a3a68f0eda45d65fe9e02d87e
Elvira <elvbom@users.noreply.github.com>
2025-04-23 15:54:12 +0200

    Fix convert consumption emissions to tons (#757)
    * convert consumption emissions to tons
    
    * update climate data
    
    * correctly convert consumption emissions to tons

--- data/issues/consumption/consumption_data_calculations.py
+++ data/issues/consumption/consumption_data_calculations.py
@@ -1,14 +1,22 @@
-import pandas as pd
 import json
+import pandas as pd
 
 
-PATH_CONSUMPTION_DATA = 'issues/consumption/consumption_data_raw.json'
+PATH_CONSUMPTION_DATA = "issues/consumption/consumption_data_raw.json"
 
 
 def get_consumption_emissions(df):
+    """Add consumption emissions data to the input DataFrame.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame containing municipality data
+
+    Returns:
+        pd.DataFrame: DataFrame with consumption emissions data added
+    """
 
     # Parse JSON data
-    with open(PATH_CONSUMPTION_DATA, 'r') as file:
+    with open(PATH_CONSUMPTION_DATA, "r", encoding="utf-8") as file:
         data = json.load(file)
 
     # List to store each municipality's emission properties
@@ -16,24 +24,27 @@ def get_consumption_emissions(df):
     total_emissions_list = []
 
     for item in data:
-        for feature in item['features']:
+        for feature in item["features"]:
             # Get properties
-            properties = feature['properties']
+            properties = feature["properties"]
 
             # Rename keys
-            properties['Kommun'] = properties.pop('kom_namn')
+            properties["Kommun"] = properties.pop("kom_namn")
 
             total_emissions_list.append(
-                {'Kommun': properties['Kommun'], 'Konsumtionsutsläpp (kg/person/år)': properties['Total emissions']})
+                {
+                    "Kommun": properties["Kommun"],
+                    "Konsumtionsutsläpp (ton/person/år)": properties["Total emissions"],
+                }
+            )
 
             # Remove unwanted 'geoid' and 'län' key
-            properties.pop('geoid', None)
-            län = properties.pop('län', None)
+            properties.pop("geoid", None)
 
             features_list.append(properties)
 
     # Convert to pandas DataFrame
     df_consumption = pd.DataFrame(features_list)
 
-    df = df.merge(df_consumption, on='Kommun', how='left')
+    df = df.merge(df_consumption, on="Kommun", how="left")
     return df
