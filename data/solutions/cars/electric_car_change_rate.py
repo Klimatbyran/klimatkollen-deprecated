89c5e778b967045e88a516407a12a643b9e298b3
elvbom <ecdb@protonmail.com>
2025-04-29 08:44:14 +0200

    update source column names and ev change script

--- data/solutions/cars/electric_car_change_rate.py
+++ data/solutions/cars/electric_car_change_rate.py
@@ -5,7 +5,8 @@ import pandas as pd
 PATH_CARS_DATA = "solutions/cars/sources/kpi1_calculations.xlsx"
 
 
-def get_electric_car_change_rate(df, to_percent: bool = False):
+def get_electric_car_change_rate(df, to_percent: bool = True):
+    """Calculate the change rate of newly registered rechargeable cars per municipality and year."""
     # LOAD AND PREP DATA ON CHANGE RATE OF PERCENTAGE OF NEWLY REGISTERED RECHARGABLE CARS PER MUNICIPALITY AND YEAR
     df_raw_cars = pd.read_excel(PATH_CARS_DATA)
 
@@ -14,13 +15,24 @@ def get_electric_car_change_rate(df, to_percent: bool = False):
     df_raw_cars = df_raw_cars.reset_index(drop=True)
 
     df_raw_cars["electricCarChangePercent"] = df_raw_cars[
-        "Procentenheter förändring av andel laddbara bilar 2015-2022"
+        "electricCarChangePercent"
     ] * (100 if to_percent else 1)
-    
-    years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+
+    years = [
+        2015,
+        2016,
+        2017,
+        2018,
+        2019,
+        2020,
+        2021,
+        2022,
+        2023,
+        2024,
+    ]  # PLEASE NOTE: this needs to be updated if the data is updated
     df_raw_cars["electricCarChangeYearly"] = df_raw_cars.apply(
         lambda x: {year: x.loc[year] * (100 if to_percent else 1) for year in years},
-        axis=1
+        axis=1,
     )
 
     df_cars = df_raw_cars.filter(
