9b13aa268dcc71c2d9eb263c3a9b9de98660fb70
elvbom <ecdb@protonmail.com>
2025-05-06 13:42:51 +0200

    add 2024 data to 2023 base, update test to match

--- data/solutions/bicycles/bicycle_data_calculations.py
+++ data/solutions/bicycles/bicycle_data_calculations.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 
-PATH_BICYCLE_DATA = 'solutions/bicycles/Cykelnät per komun 20231231.xlsx'
-PATH_POPULATION_DATA = 'solutions/bicycles/be0101_tabhel2023.xlsx'
+PATH_BICYCLE_DATA_2023 = "solutions/bicycles/Cykelnät per komun 20231231.xlsx"
+PATH_BICYCLE_DATA_2024 = "solutions/bicycles/Statistik_2024.xlsx"
+PATH_POPULATION_DATA = "solutions/bicycles/be0101_tabhel2024.xlsx"
 
 
 def calculate_bike_lane_per_capita():
@@ -16,27 +17,50 @@ def calculate_bike_lane_per_capita():
         pandas.DataFrame: A DataFrame containing the merged data and the calculated bike lane per capita.
     """
 
-    df_raw_bicycles = pd.read_excel(PATH_BICYCLE_DATA, skiprows=3)
-    df_bicycles = df_raw_bicycles[['Kommun', 'Totalsumma']]
-    df_bicycles.loc[df_bicycles['Kommun'] == 'Malung', 'Kommun'] = 'Malung-Sälen'
-    df_bicycles.loc[df_bicycles['Kommun'] == 'Upplands-Väsby', 'Kommun'] = 'Upplands Väsby'
+    df_raw_bicycles = pd.read_excel(PATH_BICYCLE_DATA_2023, skiprows=3)
+    df_bicycles = df_raw_bicycles[["Kommun", "Totalsumma"]]
+    df_bicycles.loc[df_bicycles["Kommun"] == "Malung", "Kommun"] = "Malung-Sälen"
+    df_bicycles.loc[df_bicycles["Kommun"] == "Upplands-Väsby", "Kommun"] = (
+        "Upplands Väsby"
+    )
+
+    df_bicycles_2024 = pd.read_excel(PATH_BICYCLE_DATA_2024)
+    # Group by 'Kommunnamn' and sum '_length'
+    df_2024_sum = (
+        df_bicycles_2024.groupby("Kommunnamn")["_length.sum"].sum().reset_index()
+    )
+    df_2024_sum = df_2024_sum.rename(
+        columns={"Kommunnamn": "Kommun", "_length.sum": "length_2024"}
+    )
+    # Merge 2024 sums into 2023 data
+    df_bicycles = df_bicycles.merge(df_2024_sum, on="Kommun", how="left")
+    # Add 2024 length to 2023 Totalsumma (fillna(0) in case of missing data)
+    df_bicycles["Totalsumma"] = df_bicycles["Totalsumma"] + df_bicycles[
+        "length_2024"
+    ].fillna(0)
+    # Drop the helper column
+    df_bicycles = df_bicycles.drop(columns=["length_2024"])
 
     df_raw_population = pd.read_excel(PATH_POPULATION_DATA, skiprows=5)
     # Drop unnecessary rows
     df_population_drop = df_raw_population.drop([0, 1, 2, 3])
     # Filter out county rows (that have 2 codes in the 'Kommun' column instead of 4)
-    df_population_municipality = df_population_drop[df_population_drop['Kommun'].str.len() == 4]
+    df_population_municipality = df_population_drop[
+        df_population_drop["Kommun"].str.len() == 4
+    ]
     # Filter out unnecessary columns
-    df_population_filter = df_population_municipality[['Kommunnamn', 'Folkmängd']]
+    df_population_filter = df_population_municipality[["Kommunnamn", "Folkmängd"]]
     # Rename 'Kommunnamn' to 'Kommun' to match the bicycle dataframe
-    df_population_renamed = df_population_filter.rename(columns={'Kommunnamn': 'Kommun'})
+    df_population_renamed = df_population_filter.rename(
+        columns={"Kommunnamn": "Kommun"}
+    )
     # Strip 'Kommun' column of whitespaces
-    df_population_renamed['Kommun'] = df_population_renamed['Kommun'].str.strip()
+    df_population_renamed["Kommun"] = df_population_renamed["Kommun"].str.strip()
 
     # Merge bicycle and population dataframes
-    df_merged = df_bicycles.merge(df_population_renamed, on='Kommun', how='left')
+    df_merged = df_bicycles.merge(df_population_renamed, on="Kommun", how="left")
 
     # Calculate bike lane per capita
-    df_merged['bikeMetrePerCapita'] = df_merged['Totalsumma'] / df_merged['Folkmängd']
+    df_merged["bikeMetrePerCapita"] = df_merged["Totalsumma"] / df_merged["Folkmängd"]
 
-    return df_merged[['Kommun', 'bikeMetrePerCapita']]
+    return df_merged[["Kommun", "bikeMetrePerCapita"]]
