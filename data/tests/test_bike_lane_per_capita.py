2a0aaab55f160bfc07d5a88e754140eb0df8a610
Elvira <elvbom@users.noreply.github.com>
2025-05-13 14:40:49 +0200

    feat: update bike lane data 2024 (#764)
    * add new bike lane source for 2024
    * update population statistics for 2024
    * update gitignore to not include open xlsx files
    * update bike lane test
    * collect bike lane sources in new folder
    * run data pipeline

--- data/tests/test_bike_lane_per_capita.py
+++ data/tests/test_bike_lane_per_capita.py
@@ -6,18 +6,25 @@ from solutions.bicycles.bicycle_data_calculations import calculate_bike_lane_per
 
 
 class TestBicycleCalculations(unittest.TestCase):
-    
+
     def test_calculate_bike_lane_per_capita(self):
         df_expected = pd.DataFrame(
             {
                 "Kommun": ["Ale", "Alingsås", "Alvesta"],
-                "bikeMetrePerCapita": [91548/32446, 122012/42382, 66699/20040],
+                "bikeMetrePerCapita": [
+                    92077 / 32576,
+                    123300 / 42722,
+                    66699 / 19830,
+                ],
             }
         )
 
         df_result = calculate_bike_lane_per_capita()
 
-        pd.testing.assert_frame_equal(df_result.iloc[:3], df_expected, check_dtype=False)
+        pd.testing.assert_frame_equal(
+            df_result.iloc[:3], df_expected, check_dtype=False
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
